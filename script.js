110ab314053758a7ec0ca047da7ea3280f1e6853
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-08-05 14:08:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function return_normal() {
     //Write your code here
 	let strongTags = document.querySelectorAll("p strong");
     for(let i=0;i<strongTags.length;i++){
-	 strongTags[i].style.color="rgb(0, 128, 0)";
+	 strongTags[i].style.color="rgb(0, 0, 0)";
    }
 
     
