c1b8d53bcaa7098d9c818e275a0d2d57ca7be6fe
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-08-05 12:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function highlight() {
     //Write your code here
 	let strongTags = document.querySelectorAll("p strong");
-	   strongTags.style.color=(rgb(0, 128, 0));
+	   strongTags.style.color="(rgb(0, 128, 0)";
 	
 
 
@@ -12,7 +12,7 @@ function highlight() {
 function return_normal() {
     //Write your code here
 		let strongTags = document.querySelectorAll("p strong");
-	   strongTags.style.color=(rgb(0, 0, 0));
+	   strongTags.style.color="rgb(0, 0, 0)";
 
     
 }
