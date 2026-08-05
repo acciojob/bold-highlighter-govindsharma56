f3c111ba313e3b613940b4ff67165470c86ccc47
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-08-05 12:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+
 function highlight() {
     //Write your code here
+	let strongTags = document.querySelectorAll("p strong");
+	   strongTags.style.color=(rgb(0, 128, 0));
+	
 
 
 }
@@ -7,6 +11,8 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+		let strongTags = document.querySelectorAll("p strong");
+	   strongTags.style.color=(rgb(0, 0, 0));
 
     
 }
