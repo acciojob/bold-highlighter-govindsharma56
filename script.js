9a9558ea857c127852dd1174408cf2e92fcb150b
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-08-05 14:02:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,11 +11,9 @@ function highlight() {
 function return_normal() {
     //Write your code here
 	let strongTags = document.querySelectorAll("p strong");
-	strongTags.addEventListener("mouseout", () => {
     for(let i=0;i<strongTags.length;i++){
 	 strongTags[i].style.color="(rgb(0, 128, 0)";
    }
-});
 
     
 }
